93c961f58e9d8b8e48cfa9240c814265c9201de8
Fabricio-Cagnoni <fafac_2000@hotmail.com>
2026-09-26 17:29:02 -0300

    feat: add home view to query recent open items
    Refs #8

--- items/views.py
+++ items/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from .forms import ItemForm
 from .models import Item
@@ -20,3 +20,11 @@ class ItemCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
 class ItemDetailView(DetailView):
     # select_related fetches the author in the same query
     queryset = Item.objects.select_related('author')
+class HomeView(ListView):
+    """Displays the most recent open lost and found items on the home page."""
+    model = Item
+    template_name = 'home.html'
+    context_object_name = 'recent_items'
+
+    def get_queryset(self):
+        return Item.objects.filter(status=Item.Status.OPEN).select_related('author').order_by('-created_at')[:8]
